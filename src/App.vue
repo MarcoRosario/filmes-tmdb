3969472d5529c800dcdce9a4f9552b2545a43b1f
MarcoRosario <marcoantoniorosario.academico@gmail.com>
2023-11-20 16:44:34 -0300

    LISTANDO FILMES LISTAGEM  LIVROS NÃO VAI/CHAVE APi

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import api from './axios'
+import api from './plugins/axios'
 
 const moviesGenres = ref([])
 const TVGenres = ref([])
@@ -26,5 +26,36 @@ onMounted(async () => {
       {{ genre.name }}
     </li>
   </ul>
-  
+  <header>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/filmes">Filmes</router-link>
+      <router-link to="/tv">Programas de TV</router-link>
+    </nav>
+  </header>
+  <main>
+    <router-view />
+  </main>
 </template>
+<style scoped>
+header {
+  height: 3rem;
+  display: flex;
+  background-color: black;
+  color: #fff;
+  font-size: 1.2rem;
+  padding-left: 2rem;
+}
+
+nav {
+  column-gap: 2rem;
+  margin-bottom: 0;
+  display: flex;
+  align-items: center;
+}
+
+nav a {
+  text-decoration: none;
+  color: #fff;
+}
+</style>
