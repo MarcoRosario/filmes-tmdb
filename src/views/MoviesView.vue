16e0f7f524773b7f111904952de49b7ecee7e7c6
MarcoRosario <marcoantoniorosario.academico@gmail.com>
2023-11-22 15:00:22 -0300

    Gerenciamento de estados com Pinia 2

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -2,131 +2,124 @@
 import { ref, onMounted } from 'vue'
 import api from '@/plugins/axios'
 import Loading from 'vue-loading-overlay'
-import useGenreStore from '@/stores/genre';
+import useGenreStore from '@/stores/genre'
 
 const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR')
+const movies = ref([])
 
+// function getGenreName(id) {
+//   const genero = genres.value.find((genre) => genre.id === id)
+//   return genero.name
+// }
 
-function getGenreName(id) {
-    const genero = genres.value.find((genre) => genre.id === id);
-    return genero.name;
-  }
+const isLoading = ref(false)
 
-const isLoading = ref(false);
+// const genres = ref([])
 
-const genres = ref([])
-
-const genreStore = useGenreStore();
+const genreStore = useGenreStore()
 
 const listMovies = async (genreId) => {
-  isLoading.value = true;
+  isLoading.value = true
   const response = await api.get('discover/movie', {
     params: {
       with_genres: genreId,
       language: 'pt-BR'
     }
-  });
+  })
   movies.value = response.data.results
-  isLoading.value = false;
-};
+  isLoading.value = false
+}
 
 onMounted(async () => {
-  isLoading.value = true;
-  await genreStore.getAllGenres('movie');
-  isLoading.value = false;
-});
-
+  isLoading.value = true
+  await genreStore.getAllGenres('movie')
+  isLoading.value = false
+})
 </script>
 <template>
-    <div>
-        <h1>Gêneros de filmes</h1>
-        <ul>
-            <li v-for="genre in genres" :key="genre.id">
-                {{ genre.name }}
-            </li>
-        </ul>
-        <h1>Filmes</h1>
-        <ul class="genre-list">
-            <li v-for="genre in genres" :key="genre.id" @click="listMovies(genre.id)" class="genre-item">
-                {{ genre.name }}
-            </li>
-        </ul>
-        <loading v-model:active="isLoading" is-full-page />
-        <div class="movie-list">
-            <div v-for="movie in movies" :key="movie.id" class="movie-card">
-
-                <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="movie.title" />
-                <div class="movie-details">
-                    <p class="movie-title">{{ movie.title }}</p>
-                    <p class="movie-release-date">{{ formatDate(movie.release_date) }}</p>
-                    <p class="movie-genres">
-  <span v-for="genre_id in movie.genre_ids" :key="genre_id" @click="listMovies(genre_id)">
-    {{ getGenreName(genre_id) }} 
-  </span>
-</p>
-
-                </div>
-
-            </div>
+  <div>
+    <h1>Filmes</h1>
+    <ul class="genre-list">
+      <li
+        v-for="genre in genreStore.genres"
+        :key="genre.id"
+        @click="listMovies(genre.id)"
+        class="genre-item"
+      >
+        {{ genre.name }}
+      </li>
+    </ul>
+    <loading v-model:active="isLoading" is-full-page />
+    <div class="movie-list">
+      <div v-for="movie in movies" :key="movie.id" class="movie-card">
+        <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="movie.title" />
+        <div class="movie-details">
+          <p class="movie-title">{{ movie.title }}</p>
+          <p class="movie-release-date">{{ formatDate(movie.release_date) }}</p>
+          <p class="movie-genres">
+            <span v-for="genre_id in movie.genre_ids" :key="genre_id" @click="listMovies(genre_id)">
+              {{ genreStore.getGenreName(genre_id) }}
+            </span>
+          </p>
         </div>
-
+      </div>
     </div>
-    
+  </div>
 </template>
 
 <style scoped>
 .genre-list {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    gap: 2rem;
-    list-style: none;
-    padding: 0;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  list-style: none;
+  padding: 0;
 }
 
 .genre-item {
-    background-color: #387250;
-    border-radius: 1rem;
-    padding: 0.5rem 1rem;
-    color: #fff;
+  background-color: #387250;
+  border-radius: 1rem;
+  padding: 0.5rem 1rem;
+  color: #fff;
 }
 
 .genre-item:hover {
-    cursor: pointer;
-    background-color: #4e9e5f;
-    box-shadow: 0 0 0.5rem #387250;
+  cursor: pointer;
+  background-color: #4e9e5f;
+  box-shadow: 0 0 0.5rem #387250;
 }
 
 .movie-list {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 1rem;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
 }
 
 .movie-card {
-    width: 15rem;
-    height: 30rem;
-    border-radius: 0.5rem;
-    overflow: hidden;
-    box-shadow: 0 0 0.5rem #000;
+  width: 15rem;
+  height: 30rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  box-shadow: 0 0 0.5rem #000;
 }
 
 .movie-card img {
-    width: 100%;
-    height: 20rem;
-    border-radius: 0.5rem;
-    box-shadow: 0 0 0.5rem #000;
+  width: 100%;
+  height: 20rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0.5rem #000;
 }
 
 .movie-details {
-    padding: 0 0.5rem;
+  padding: 0 0.5rem;
 }
 
 .movie-title {
-    font-size: 1.1rem;
-    font-weight: bold;
-    line-height: 1.3rem;
-    height: 3.2rem;
+  font-size: 1.1rem;
+  font-weight: bold;
+  line-height: 1.3rem;
+  height: 3.2rem;
 }
 .movie-genres {
   display: flex;
@@ -151,6 +144,4 @@ onMounted(async () => {
   background-color: #455a08;
   box-shadow: 0 0 0.5rem #748708;
 }
-
-
-}</style>
+</style>
